a61b03442852b977d854811899bb41fab5c60e85
Santo Khan <santokhan1999@gmail.com>
2024-03-26 12:05:09 +0600

    update sidebar

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { sidebarNavs } from '../utils/sidebar-navs';
 import { Link, useLocation } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 const Sidebar = () => {
     const location = useLocation();
+    const pathName = location.pathname;
 
     return (
         <div>
@@ -12,7 +14,10 @@ const Sidebar = () => {
                     const { label, to } = item;
                     return (
                         <li key={index}>
-                            <Link to={to} className='block py-2.5 px-4 rounded-lg font-medium hover:bg-gray-100'>{label}</Link>
+                            <Link to={to} className={twMerge(
+                                'block py-2.5 px-4 rounded-lg font-medium',
+                                pathName == to ? 'bg-gray-800 text-gray-50' : "text-gray-800 hover:bg-gray-100"
+                            )}>{label}</Link>
                         </li>
                     );
                 })}
