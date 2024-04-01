248be70a0c42feac08380080af4464efe521b9cf
Santo Khan <santokhan1999@gmail.com>
2024-04-01 15:43:30 +0600

    update metadata

--- src/components/action-buttons/Edit.jsx
+++ src/components/action-buttons/Edit.jsx
@@ -2,7 +2,7 @@ import { Edit } from "iconsax-react";
 
 const ActionEdit = ({ onEdit }) => {
     return (
-        <button type="button" className="font-medium rounded-full text-sm text-center inline-flex items-center hover:text-green-500" onClick={onEdit}>
+        <button type="button" className="font-medium rounded-full text-sm text-center inline-flex items-center hover:text-green-600" onClick={onEdit}>
             <Edit className="w-5 h-5" />
         </button>
     );
