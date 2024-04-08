0fa1b74332201b4ad9d39126a51f19bca0875129
Santo Khan <santokhan1999@gmail.com>
2024-04-08 11:36:53 +0600

    update aminity form handling

--- src/blocks/form/properties/AmenitiesForm.jsx
+++ src/blocks/form/properties/AmenitiesForm.jsx
@@ -1,60 +1,159 @@
 import { useForm } from "react-hook-form";
 import CancelOrSubmit from "../../../components/form/CancelOrSubmit";
-import InputBox from "../../../components/form/InputBox";
 import { useNavigate } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
-import SubmitButton, { FinalSubmitButton } from "../../../components/form/SubmitButton";
+import { FinalSubmitButton } from "../../../components/form/SubmitButton";
+import { useState } from "react";
 
-function CheckBoxContainer({ amenity }) {
+const amenities = [
+    {
+        "id": "attic",
+        "name": "Attic"
+    },
+    {
+        "id": "basketball_court",
+        "name": "Basketball court"
+    },
+    {
+        "id": "air_conditioning",
+        "name": "Air Conditioning"
+    },
+    {
+        "id": "lawn",
+        "name": "Lawn"
+    },
+    {
+        "id": "swimming_pool",
+        "name": "Swimming Pool"
+    },
+    {
+        "id": "barbeque",
+        "name": "Barbeque"
+    },
+    {
+        "id": "microwave",
+        "name": "Microwave"
+    },
+    {
+        "id": "tv_cable",
+        "name": "TV Cable"
+    },
+    {
+        "id": "dryer",
+        "name": "Dryer"
+    },
+    {
+        "id": "outdoor_shower",
+        "name": "Outdoor Shower"
+    },
+    {
+        "id": "washer",
+        "name": "Washer"
+    },
+    {
+        "id": "ocean_view",
+        "name": "Ocean view"
+    },
+    {
+        "id": "private_space",
+        "name": "Private space"
+    },
+    {
+        "id": "lake_view",
+        "name": "Lake view"
+    },
+    {
+        "id": "wine_cellar",
+        "name": "Wine cellar"
+    },
+    {
+        "id": "front_yard",
+        "name": "Front yard"
+    },
+    {
+        "id": "refrigerator",
+        "name": "Refrigerator"
+    },
+    {
+        "id": "wifi",
+        "name": "WiFi"
+    },
+    {
+        "id": "laundry",
+        "name": "Laundry"
+    },
+    {
+        "id": "sauna",
+        "name": "Sauna"
+    }
+];
+
+const initialValue = {
+    id: "",
+    name: ""
+}
+
+function CheckBoxContainer({ amenity, onChange, valueObj }) {
+    if (!valueObj) {
+        valueObj = initialValue
+    }
+
     return (
         <div key={amenity.id} className="flex gap-x-2 items-center">
-            <input id={amenity.id} name={amenity.id} type="checkbox" className="h-4 w-4 rounded-lg border-gray-300" />
-            <label htmlFor={amenity.id} className="font-medium text-gray-900">{amenity.name}</label>
+            <input
+                id={amenity.id}
+                name={amenity.id} type="checkbox"
+                className="h-4 w-4 rounded-lg border-gray-300"
+                onChange={onChange}
+                value={valueObj.id}
+            />
+            <label
+                htmlFor={amenity.id}
+                className="font-medium text-gray-900"
+            >{amenity.name}</label>
         </div>
     );
 }
 
-const amenities = [
-    { "id": "attic", "name": "Attic" },
-    { "id": "basketball_court", "name": "Basketball court" },
-    { "id": "air_conditioning", "name": "Air Conditioning" },
-    { "id": "lawn", "name": "Lawn" },
-    { "id": "swimming_pool", "name": "Swimming Pool" },
-    { "id": "barbeque", "name": "Barbeque" },
-    { "id": "microwave", "name": "Microwave" },
-    { "id": "tv_cable", "name": "TV Cable" },
-    { "id": "dryer", "name": "Dryer" },
-    { "id": "outdoor_shower", "name": "Outdoor Shower" },
-    { "id": "washer", "name": "Washer" },
-    { "id": "ocean_view", "name": "Ocean view" },
-    { "id": "private_space", "name": "Private space" },
-    { "id": "lake_view", "name": "Lake view" },
-    { "id": "wine_cellar", "name": "Wine cellar" },
-    { "id": "front_yard", "name": "Front yard" },
-    { "id": "refrigerator", "name": "Refrigerator" },
-    { "id": "wifi", "name": "WiFi" },
-    { "id": "laundry", "name": "Laundry" },
-    { "id": "sauna", "name": "Sauna" }
-];
-
 const AmenitiesForm = () => {
-    const { register, handleSubmit, errors } = useForm();
+    // const { register, handleSubmit, errors } = useForm();
     const navigate = useNavigate()
+    const [formState, setFormState] = useState([])
 
-    const onSubmit = (data) => {
-        console.log(data); // Handle form submission here
+    const onSubmit = (e) => {
+        const formData = new FormData(e.target);
 
-        // navigate("/properties/create/media")
+        // store formData to context
+
+        navigate("/properties/create/media")
     };
 
     return (
-        <form className='' onSubmit={handleSubmit(onSubmit)}>
+        <form className="" onSubmit={onSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
                 {amenities.map(amenity => (
-                    <CheckBoxContainer key={amenity.id} amenity={amenity} />
+                    <CheckBoxContainer
+                        key={amenity.id}
+                        amenity={amenity}
+                        onChange={() => {
+                            // Check if the amenity already exists in formState based on amenity.id
+                            const existingAmenity = formState.find(item => item.id === amenity.id);
+
+                            if (existingAmenity) {
+                                // If amenity with the same id already exists, perform filtering
+                                const filteredFormState = formState.filter(item => item.id !== amenity.id);
+                                setFormState(filteredFormState);
+                            } else {
+                                // If amenity doesn't exist in formState, add it
+                                setFormState([...formState, amenity]);
+                            }
+                        }}
+                        valueObj={formState.find(({ id }) => id === amenity.id)}
+                    />
                 ))}
             </div>
             <FinalSubmitButton />
+            {/* <pre>{JSON.stringify(formState, null, 2)}</pre> */}
         </form>
     );
 };
