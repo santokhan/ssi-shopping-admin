2babf4bdfc5c28ec4e67277194a477e6eb190f96
santokhan <santokhan1999@gmail.com>
2024-04-26 10:07:41 +0600

    create agents context

---
+++ src/context/AgentsContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useEffect, useState } from 'react';
+import useAxios from './useAxios';
+
+export const AgentsContext = createContext(null);
+
+export const AgentsProvider = ({ children }) => {
+  const [agents, setAgents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { api } = useAxios();
+
+  const fetchAgents = async () => {
+    try {
+      setLoading(true);
+      const response = await api.get('agents/');
+      console.log(response.data);
+      setAgents(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAgents();
+  }, []);
+
+  return (
+    <AgentsContext.Provider value={{ agents, loading, refetch: fetchAgents }}>
+      {children}
+    </AgentsContext.Provider>
+  );
+};
