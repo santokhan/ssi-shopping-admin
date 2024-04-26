ca1dc02612e90e70952722138bba0830024384c3
santokhan <santokhan1999@gmail.com>
2024-04-26 13:18:35 +0600

    fix bug

--- src/components/form/input/AgentImageInput.jsx
+++ src/components/form/input/AgentImageInput.jsx
@@ -1,22 +1,38 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from '../../Button';
 import RemoveImage from '../../action-buttons/RemoveImage';
 import { User } from 'iconsax-react';
+import useAxios from '../../../context/useAxios';
 
 const PreviewImage = ({ src = '' }) => {
-  return (
+  return src.length > 0 ? (
     <img
       src={src}
       alt="agent-image-input"
       className="w-full h-full object-cover rounded-xl"
     />
+  ) : (
+    <User className="w-16 lg:w-20 h-16 lg:h-20 m-auto text-gray-400" />
   );
 };
 
-const AgentImageInput = ({ src = '', onChangeImage }) => {
+const AgentImageInput = ({
+  src = '',
+  agentId = null,
+  onChangeImage = (file) => {},
+  API_URL = '',
+  required = false,
+}) => {
   const [imageSrc, setImageSrc] = useState(src);
   const [inputImage, setInputImage] = useState(null);
   const fileInputRef = useRef(null);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    if (agentId || src) {
+      setImageSrc(src);
+    }
+  }, [src]);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -58,19 +74,32 @@ const AgentImageInput = ({ src = '', onChangeImage }) => {
   };
 
   const handleRemoveClick = () => {
-    setInputImage(null);
-    setImageSrc(null);
+    if (agentId && API_URL) {
+      // api
+      //   .patch(API_URL, { photo: '' })
+      //   .then((res) => {
+      //     console.log(res);
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //   })
+      //   .finally(() => {
+      //     setInputImage('');
+      //     setImageSrc('');
+      //   });
+    } else {
+      setInputImage('');
+      setImageSrc('');
+    }
   };
 
   return (
     <div className="flex items-end gap-4 rounded-lg text-gray-800">
       <div className="h-28 w-28 sm:h-32 sm:w-32 flex-shrink-0 relative bg-gray-50 rounded-xl border grid place-items-center">
-        {inputImage || src ? <RemoveImage onRemove={handleRemoveClick} /> : ''}
-        {imageSrc ? (
-          <PreviewImage src={imageSrc} />
-        ) : (
-          <User className="w-16 lg:w-20 h-16 lg:h-20 m-auto text-gray-400" />
+        {(inputImage || imageSrc) && (
+          <RemoveImage onRemove={handleRemoveClick} />
         )}
+        <PreviewImage src={imageSrc} />
       </div>
       <div className="space-y-2">
         <div className="relative">
@@ -84,7 +113,8 @@ const AgentImageInput = ({ src = '', onChangeImage }) => {
             accept=".png,.jpg,.jpeg,.webp,.gif"
             onChange={handleFileChange}
             className="opacity-0 absolute inset-0"
-            required
+            required={required}
+            defaultValue={''}
           />
         </div>
         <p>{'Photos must be JPEG or PNG format and least 2048x2048'}</p>
