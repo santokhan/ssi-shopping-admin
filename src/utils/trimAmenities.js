e570ac71ef9ab247f8ef4ead81cb15c28a3a5c27
santokhan <santokhan1999@gmail.com>
2024-04-27 15:28:28 +0600

    add trimming function

---
+++ src/utils/trimAmenities.js
@@ -0,0 +1,26 @@
+/**
+ * Remove all other keys from the object of array
+ * 
+ * @param {*} amenities 
+ * @returns 
+ */
+function trimAmenities(amenities) {
+    if (Array.isArray(amenities)) {
+        return amenities.map(({ id }) => id).flat().filter(id => id);
+    } else {
+        return [];
+    }
+}
+
+// // Example usage
+// const amenities = [
+//     {
+//         id: 1,
+//         label: "example label",
+//         icon: 'https://picsum.photos/200/300',
+//     }
+// ]
+// console.log(trimAmenities(amenities));
+// // Output: [1]
+
+export default trimAmenities;
