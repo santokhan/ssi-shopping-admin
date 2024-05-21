ee29251e116d07529f559df3563df1382b4cc2c1
santokhan <santokhan1999@gmail.com>
2024-05-22 01:56:13 +0600

    fix the google map issue

--- src/components/google-map/GoogleMap1.jsx
+++ src/components/google-map/GoogleMap1.jsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
-import Print from '../Print';
 
 const PinMarker = ({ pin }) => {
   return (
     <div
       style={{
         position: 'absolute',
-        left: pin.x ? `${pin.x}px` : '50%',
-        top: pin.y ? `${pin.y}px` : '50%',
+        left: pin?.x ? `${pin?.x}px` : '50%',
+        top: pin?.y ? `${pin?.y}px` : '50%',
         transform: 'translate(-50%, -50%)',
         height: '20px',
         width: '20px',
@@ -20,51 +18,31 @@ const PinMarker = ({ pin }) => {
   );
 };
 const GoogleMap = ({ setPosition = ({ lat, lng }) => {}, value }) => {
-  // Initialize pin state with dummy coordinates (e.g., Dubai Marina)
-  const initialPin = {
-    lat: value && value.lat ? parseFloat(value.lat) : 25.0805,
-    lng: value && value.lng ? parseFloat(value.lng) : 55.1403,
-  };
-  const [pin, setPin] = useState(initialPin);
-
   const handleMapClick = (newPin) => {
-    const { lat, lng, x, y } = newPin;
+    const { lat, lng } = newPin;
     if (lat || lng) {
-      setPin(newPin);
-      setPosition(newPin);
+      setPosition({ lat, lng });
     }
   };
 
-  useEffect(() => {
-    setPin((prev) => {
-      return {
-        ...prev,
-        lat: parseFloat(value.lat),
-        lng: parseFloat(value.lng),
-      };
-    });
-  }, [value]);
-
   return (
-    <>
-      <div
-        style={{ width: '100%', height: '50vh' }}
-        className="bg-gray-50 relative"
-      >
-        <GoogleMapReact
-          bootstrapURLKeys={{
-            key: 'AIzaSyBI33ERGnuYC9n-9K5f9gM1Kz0fQ9V8VhQ',
-          }}
-          defaultCenter={initialPin}
-          center={pin}
-          defaultZoom={10}
-          onClick={handleMapClick}
-          onGoogleApiLoaded={handleMapClick}
-          onChange={handleMapClick}
-        />
-        <PinMarker pin={pin} />
-      </div>
-    </>
+    <div
+      style={{ width: '100%', height: '50vh' }}
+      className="bg-gray-50 relative"
+    >
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: 'AIzaSyBI33ERGnuYC9n-9K5f9gM1Kz0fQ9V8VhQ',
+        }}
+        center={{
+          lat: value.lat || 25.0805,
+          lng: value.lng || 55.1403,
+        }}
+        defaultZoom={10}
+        onClick={handleMapClick}
+      />
+      <PinMarker />
+    </div>
   );
 };
 
