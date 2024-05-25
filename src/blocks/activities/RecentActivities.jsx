26f899cb1b9248ec9e6483387ba14455d893d1b0
santokhan <santokhan1999@gmail.com>
2024-05-26 00:44:13 +0600

    update dashboard activities

--- src/blocks/activities/RecentActivities.jsx
+++ src/blocks/activities/RecentActivities.jsx
@@ -1,41 +1,52 @@
-import { Heart, Home2, People, TrendUp } from 'iconsax-react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxios from '../../context/useAxios';
 
 const Notification = () => {
-    const notifications = [
-        {
-            title: 'Your listing House on the beverly hills has been approved',
-            icon: <Home2 className="h-5 w-5" />
-        },
-        {
-            title: 'Your listing House on the beverly hills has been approved',
-            icon: <Heart className="h-5 w-5" />
-        },
-        {
-            title: 'Your listing House on the beverly hills has been approved',
-            icon: <TrendUp className="h-5 w-5" />
-        },
-        {
-            title: 'Your listing House on the beverly hills has been approved',
-            icon: <People className="h-5 w-5" />
-        },
-    ]
+  const activitiesPlaceholder = [
+    'You have created a new user account',
+    'You have updated your profile information',
+    'You have deleted a post',
+    'You have added a new comment',
+    'You have uploaded a profile picture',
+    'You have changed your password',
+    'You have subscribed to a newsletter',
+    'You have unsubscribed from email notifications',
+    'You have joined a new group',
+    'You have left a group',
+  ];
+  const [activities, setActivities] = useState([]);
+  const { api } = useAxios();
 
-    return (
-        <div className="rounded-xl bg-white p-4 sm:p-6 lg:p-8 space-y-4 mt-8 shadow">
-            <div className="text-md font-semibold text-gray-700">Notifications</div>
-            <div className="space-y-3">
-                {notifications.map((notification, index) => (
-                    <div key={index} className="flex items-center space-x-3">
-                        <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-100">
-                            {notification.icon}
-                        </div>
-                        <p className="text-gray-800">{notification.title}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
+  useEffect(() => {
+    api
+      .get('activities/')
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          setActivities(data);
+        }
+      })
+      .catch((err) => {
+        setActivities(activitiesPlaceholder);
+      });
+  }, []);
+
+  return (
+    <section className="rounded-xl bg-white p-4 sm:p-6 lg:p-8 space-y-4 mt-8 shadow">
+      <div className="text-xl font-bold text-gray-700">Recent Activity</div>
+      <div className="relative">
+        <aside className="border-l-2 absolute left-[5px] top-0 bottom-0 border-dark-blue-500/50"></aside>
+        <ul className="space-y-3 relative">
+          {activities.slice(0, 12).map((_, i) => (
+            <li key={i} className="flex items-center space-x-3">
+              <div className="flex size-3 shrink-0 items-center justify-center rounded-lg border-2 bg-white border-dark-blue-500"></div>
+              <p className="text-gray-800">{_}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Notification;
