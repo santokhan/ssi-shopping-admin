0997148337afab764f370832888f04b176e79dbb
santokhan <santokhan1999@gmail.com>
2024-05-28 18:58:10 +0600

    make decimal

--- src/blocks/form/projects/DetailsForm.jsx
+++ src/blocks/form/projects/DetailsForm.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Input from '../../../components/form/input/Input';
-import Textarea from '../../../components/form/input/Textarea';
-import Select from '../../../components/form/input/SelectOption';
 import ResponsiveForm from '../../../components/form/ResponsiveForm';
 import PrevAndNext from '../../../components/form/CancelOrSubmit';
 import { formBack, formNext } from '../../../utils/form-steps';
-import { yesNoOptions } from '../../../utils/yes-no-options';
 import {
   AmenitiesGrid,
   CheckBoxContainer,
@@ -19,6 +16,7 @@ const DetailsForm = ({ value, setValue }) => {
   const { pathname } = useLocation();
 
   const [features, setFeatures] = useState([{ label: '', value: '' }]);
+
   useEffect(() => {
     getFeatures().then((res) => {
       if (res.data) {
@@ -28,37 +26,6 @@ const DetailsForm = ({ value, setValue }) => {
     });
   }, []);
 
-  const inputs = [
-    {
-      name: 'size',
-      label: 'total area (in ft)',
-      type: 'number',
-    },
-  ];
-
-  const options = {
-    structure_type: [
-      {
-        label: 'Yes',
-        value: 'yes',
-      },
-      {
-        label: 'No',
-        value: 'no',
-      },
-    ],
-    floor_no: [
-      {
-        label: '5',
-        value: '5',
-      },
-      {
-        label: '10',
-        value: '10',
-      },
-    ],
-  };
-
   return (
     <ResponsiveForm
       onSubmit={(e) => {
@@ -66,85 +33,17 @@ const DetailsForm = ({ value, setValue }) => {
         navigate(formNext(pathname));
       }}
     >
-      {inputs.map(({ name, label, type }) => {
-        if (['text', 'number'].includes(type)) {
-          return (
-            <Input
-              key={name}
-              type={type}
-              name={name}
-              label={label}
-              value={value[name]}
-              onChange={(e) => {
-                let v = e.target.value;
-                if (type === 'number') {
-                  v = parseInt(v);
-                }
-                setValue(name, v);
-              }}
-              required={
-                ['garage_size', 'year_built'].includes(name) ? false : true
-              }
-            />
-          );
-        }
-        if (type == 'date') {
-          return (
-            <Input
-              key={name}
-              type={type}
-              name={name}
-              label={label}
-              value={value[name]}
-              onChange={(e) => {
-                setValue(name, e.target.value);
-              }}
-              placeholder={type === 'date' && 'YYYY'}
-              min={(type = 'date' && 1900)}
-              max={(type = 'date' && 2100)}
-              step={(type = 'date' && 1)}
-              required
-            />
-          );
-        }
-        if (type === 'select') {
-          return (
-            <Select
-              key={name}
-              name={name}
-              label={label}
-              options={options[name]}
-              value={value[name]}
-              onChange={(e) => setValue(name, e.target.value)}
-              required
-            />
-          );
-        }
-        if (type === 'yes/no') {
-          return (
-            <Select
-              key={name}
-              name={name}
-              label={label}
-              options={yesNoOptions}
-              value={value[name]}
-              onChange={(e) => setValue(name, e.target.value)}
-            />
-          );
-        }
-        if (type === 'textarea') {
-          return (
-            <Textarea
-              key={name}
-              name={name}
-              label={label}
-              value={value[name]}
-              onChange={(e) => setValue(name, e.target.value)}
-              className="col-span-full"
-            />
-          );
-        }
-      })}
+      <Input
+        type="number"
+        name="size"
+        label="total area (in ft)"
+        value={value.size}
+        step="any"
+        onChange={(e) => {
+          setValue(e.target.name, parseFloat(e.target.value));
+        }}
+        required={true}
+      />
       <AmenitiesGrid className="col-span-full">
         {features.map((_) => {
           // remove duplicates
@@ -154,7 +53,6 @@ const DetailsForm = ({ value, setValue }) => {
             }
           };
           let clone = amenitiesReducer(value.features);
-          console.log(clone);
 
           return (
             <CheckBoxContainer
