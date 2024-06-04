5c4fa199d487df4bb6f98c97d8f7598a544495f3
santokhan <santokhan1999@gmail.com>
2024-06-04 15:00:32 +0600

    add real admin name

--- src/blocks/DashboardContent.jsx
+++ src/blocks/DashboardContent.jsx
@@ -1,12 +1,22 @@
+import { useContext } from 'react';
 import RecentActivities from '../blocks/activities/RecentActivities';
 import StatusCard from '../blocks/status/StatusCard';
+import { AuthContext } from '../context/auth-context';
+import { jwtDecode } from 'jwt-decode';
 
 const DashboardContent = () => {
+  const { token } = useContext(AuthContext);
+  const access = token?.access || '';
+  const decoded = jwtDecode(access);
+
   return (
     <div className="">
       <div className="">
-        <h3 className="text-3xl lg:text-4xl font-semibold text-gray-900">
-          Howdy, Arshad!
+        <h3 className="text-3xl lg:text-4xl font-semibold text-gray-900 capitalize">
+          Welcome,{' '}
+          {decoded?.username && (
+            <span title={decoded.email}>{decoded.username}</span>
+          )}
         </h3>
         <p className="text-base text-gray-700 lg:mt-2 font-medium">
           We are glad to see you again!
